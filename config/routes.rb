285d7f2fbae15ecea39db0eb309c15971cd0502e
masao <masao>
2020-10-06 22:44:17 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'login#index' # ここを追記します
+  get 'login/index' # 自動で設定されたルーティング
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
